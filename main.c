2992a63eab5356cfe58132b0ee73e95683f7dc09
KavinPratap <kavin.pratap@gmail.com>
2025-11-12 20:16:40 +0000

    pause

--- main.c
+++ main.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-int orith(int x, int y){
-    if(x >= y){
-        return 1;
-    } else {
-        return 0;
-    }
-}
+#include <termios.h>
+#include <unistd.h>
 
-
-
-int main() {
-    int x, y;
-    
-    printf("Enter two numbers, enter space between numbers: ");
-    scanf("%d %d", &x, &y);
-
-    int isGreater = orith(x,y);
-
-    if (isGreater == 1) {
-        printf("your first number is greater than your last\n");
-    } else {
-        printf("your first number is not greater than your last\n");
+//This code is for the controller
+//Joystick Name
+//Websockets server 
+int main(){
+    while (1) {
+        /*future code*/
+        char ch;
+        int distance;
+  
+        printf("Enter direction(W,A,S,D) left(A) and right(D) will be degrees but not yet");
+        ch = getchar();
+        printf("Enter a distance(In cm): ");
+        scanf("%d",&distance);
+        printf("This is the distance in cm with int: %d\n\n",distance );
+        
     }
     
+  
 
+    
+        // printf("Enter a key: ");
+        // while ((ch = getchar()) != 'q') {
+        //     printf("You are pressing the key %c\n", ch);
+        // }
 
     return 0;
 }
